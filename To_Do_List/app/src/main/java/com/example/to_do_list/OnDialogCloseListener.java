54948357499840cd36dcbc73c1b94834e424aade
Mohip Ghosh <mohipghosh6@gmail.com>
2026-07-27 22:33:22 +0530

    Initial Commit - Codesoft Task 1

---
+++ To_Do_List/app/src/main/java/com/example/to_do_list/OnDialogCloseListener.java
@@ -0,0 +1,11 @@
+package com.example.to_do_list;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListener {
+
+    void onDialogClose(DialogInterface dialogInterface);
+}
+
+
+
